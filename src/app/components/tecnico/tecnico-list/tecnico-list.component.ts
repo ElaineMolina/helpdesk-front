8cfacdf3df978b1eff0e8da822dfdd7277c7ad89
Elaine Molina <ElaineMolina>
2024-04-05 18:36:00 -0300

    tecnico-list table part 1

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from './models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+    id:  1,
+    nome: 'Sheegwa Molina',
+    cpf: '123.1223.123-10',
+    email: 'sheegwa.molina@mail.com',
+    senha: '123',
+    perfis: ['0'],
+    dataCriacao: '05/04/2024'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
