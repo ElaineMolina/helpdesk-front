bd36ad1758e3b07997c69eae688f40a0977abbd7
Elaine Molina <ElaineMolina>
2024-04-19 08:38:03 -0300

    filter and method for CHAMADO

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,39 +1,73 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Chamado } from '../../models/chamado';
+import { ChamadoService } from 'src/app/services/chamado.service';
 
 @Component({
   selector: 'app-chamado-list',
   templateUrl: './chamado-list.component.html',
   styleUrls: ['./chamado-list.component.css']
 })
-export class ChamadoListComponent {
+export class ChamadoListComponent implements OnInit {
 
-  ELEMENT_DATA: Chamado[] = [
-    {
-      id: 1,
-      dataAbertura: '02/06/2023',
-      dataFechamento: '02/06/2023',
-      prioridade: 'Alta',
-      status: 'ANDAMENTO',
-      titulo: 'Chamado 1',
-      descricao: 'Teste chamado 1',
-      tecnico: 1,
-      cliente: 6,
-      nomeCliente: 'Elaine Molina',
-      nomeTecnico: 'Mascara',
-    }
-  ];
+  constructor(
+    private service: ChamadoService
+  ) { }
 
-  displayedColumns: string[] = ['id', 'titulo',  'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  ELEMENT_DATA: Chamado[] = [];
+  FILTERED_DATA: Chamado[] = [];
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
   dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-	applyFilter(event: Event) {
+  applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  findAll(): void {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(resposta);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  retornaStatus(status: any): string {
+    if (status == '0') {
+      return 'ABERTO'
+    } else if (status == '1') {
+      return 'ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == '0') {
+      return 'BAIXA'
+    } else if (prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+
+  ordersByStatus(status: any): void {
+    let list: Chamado[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if (element.status == status)
+        list.push(element)
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list); // or (this.FILTERED_DATA)
+    this.dataSource.paginator = this.paginator;
+  }
 }
---
+++ src/app/services/chamado.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Chamado } from '../components/models/chamado';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChamadoService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  findAll(): Observable<Chamado[]> {
+    return  this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
+  }
+}
